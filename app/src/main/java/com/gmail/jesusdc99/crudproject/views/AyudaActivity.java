7de7027169b533c3261360a2b6c44e40f1670491
jesusdc99 <36838479+jesusdc99@users.noreply.github.com>
2020-02-28 10:54:15 +0100

    Creada actividad Ayuda

---
+++ app/src/main/java/com/gmail/jesusdc99/crudproject/views/AyudaActivity.java
@@ -0,0 +1,16 @@
+package com.gmail.jesusdc99.crudproject.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gmail.jesusdc99.crudproject.R;
+
+public class AyudaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda);
+    }
+}
